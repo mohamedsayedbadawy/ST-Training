f9d703cde5cc6f668f0903fd1c3d2b1a1eec0f8b
mohamedsayedbadawy <138709186+mohamedsayedbadawy@users.noreply.github.com>
2024-08-02 23:00:47 +0300

    Delete st training directory

--- st training/session two/task one/function.c
+++
@@ -1,9 +0,0 @@
-#include"function.h"
-#include"stdio.h"
-void print_data(int salary1,int salary2,int salary3)
-{
-	printf("your expected salary is=  %d\n",salary1);
-	printf("your real salayr is = %d \n",salary2);
-	printf("your expected inreasing is = %d\n",salary3);
-
-}
--- st training/session two/task one/function.h
+++
@@ -1,4 +0,0 @@
-#ifndef _H_A
-#define _H_A
-void print_data(int salary1,int salary2,int salary3);
-#endif
